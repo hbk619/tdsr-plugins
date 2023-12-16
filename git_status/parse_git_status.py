fd95252e4d81e33df5c60f97d27030d40729804b
hbk619 <hbk619@users.noreply.github.com>
2023-12-16 11:09:26 +0000

    feat: Tell the user when there are unstaged files.
    This is a bit of a lame implementation but I just want it to work for
    now, I'm sure we can make it nicer to read later

--- git_status/parse_git_status.py
+++ git_status/parse_git_status.py
@@ -2,19 +2,34 @@ import re
 
 instructions = re.compile(r"\(use\s\"git\s.*\)")
 staged = re.compile(r"Changes to be committed:")
+unstaged = re.compile(r"Changes not staged for commit:")
 status = re.compile(r"\s*(modified|added|removed|new file|renamed):.*")
 
 
 def parse_output(lines):
     in_staged = False
-    changes = []
+    in_unstaged = False
+    staged_files = []
+    unstaged_files = []
     lines.reverse()
     for line in lines:
         if staged.match(line):
             in_staged = True
+            staged_files.append("Staged")
+        elif unstaged.match(line):
+            in_unstaged = True
+            unstaged_files.append("Not staged")
         elif in_staged:
             if status.match(line):
-                changes.append(line.strip())
+                staged_files.append(line.strip())
             elif len(line) == 0:
-                break
-    return changes if len(changes) else ['No staged files']
+                in_staged = False
+        elif in_unstaged:
+            if status.match(line):
+                unstaged_files.append(line.strip())
+            elif len(line) == 0:
+                in_unstaged = False
+
+    staged_files = ['No staged files'] if len(staged_files) == 0 else staged_files
+    unstaged_files = ['No unstaged files'] if len(unstaged_files) == 0 else unstaged_files
+    return staged_files + unstaged_files
--- git_status/test_git_status.py
+++ git_status/test_git_status.py
@@ -18,11 +18,13 @@ some/folder/thing/untracked.md"""
 
 
 def test_git_status_committed():
-    expected = ["modified:   ma/path/to/staged.md"]
+    expected = ["Staged", "modified:   ma/path/to/staged.md", "Not staged", "modified:   a_modified_file",
+                "modified:   path/to/a_modified_file"]
     lines = modified_staged.split('\n')
     lines.reverse()
     results = parse_output(lines)
     assert results == expected
+
 
 various_staged = """On branch my-branchie
 Changes to be committed:
@@ -34,7 +36,7 @@ removed: ma/deleted/file.py
 Changes not staged for commit:
 (use "git add <file>..." to update what will be committed)
 (use "git restore <file>..." to discard changes in working directory)
-modified:   a_modified_file
+removed:   a_modified_file
 modified:   path/to/a_modified_file
 
 Untracked files:
@@ -44,13 +46,16 @@ some/folder/thing/untracked.md"""
 
 
 def test_git_status_staged_renamed_added_removed():
-    expected = ["renamed:   ma/path/to/staged.md -> ma/path/to/staged-test.md", "added: ma/new/file.py", "removed: ma/deleted/file.py"]
+    expected = ["Staged", "renamed:   ma/path/to/staged.md -> ma/path/to/staged-test.md", "added: ma/new/file.py",
+                "removed: ma/deleted/file.py", "Not staged", "removed:   a_modified_file",
+                "modified:   path/to/a_modified_file"]
     lines = various_staged.split('\n')
     lines.reverse()
     results = parse_output(lines)
     assert results == expected
 
-no_changes = """On branch my-branchie
+
+no_staged_changes = """On branch my-branchie
 Your branch is ahead of 'origin/master' by 2 commits.
   (use "git push" to publish your local commits)
 
@@ -66,8 +71,26 @@ Untracked files:
 some/folder/thing/untracked.md"""
 
 
+def test_git_status_committed_no_staged_changes():
+    expected = ["No staged files", "Not staged", "modified:   a_modified_file", "modified:   path/to/a_modified_file"]
+    lines = no_staged_changes.split('\n')
+    lines.reverse()
+    results = parse_output(lines)
+    assert results == expected
+
+
+no_changes = """On branch my-branchie
+Your branch is ahead of 'origin/master' by 2 commits.
+  (use "git push" to publish your local commits)
+
+Untracked files:
+(use "git add <file>..." to include in what will be committed)
+.hidden
+some/folder/thing/untracked.md"""
+
+
 def test_git_status_committed_no_changes():
-    expected = ["No staged files"]
+    expected = ["No staged files", "No unstaged files"]
     lines = no_changes.split('\n')
     lines.reverse()
     results = parse_output(lines)
