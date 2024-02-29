7cfdc0350052876f9660a4a2f147ca22e5bc12dd
hbk619 <hbk619@users.noreply.github.com>
2024-02-29 11:55:01 +0000

    fix: Write log file to home dir
    Otherwise we write it where ever you launch TDSR which is a bit annoying!

--- pluginlogging/logger.py
+++ pluginlogging/logger.py
@@ -1,11 +1,12 @@
 import logging
 from logging.handlers import RotatingFileHandler
+import os
 
 logger = logging.getLogger('hbk619-plugins')
 logger.setLevel(logging.INFO)
 
 FIVE_MEG_IN_BYTES = 5000000
-handler = RotatingFileHandler('hbk619-plugins.log', maxBytes=FIVE_MEG_IN_BYTES, backupCount=1)
+handler = RotatingFileHandler(os.path.join(os.getenv('HOME'), 'hbk619-plugins.log'), maxBytes=FIVE_MEG_IN_BYTES, backupCount=1)
 handler.setLevel(logging.INFO)
 
 logger.addHandler(handler)
